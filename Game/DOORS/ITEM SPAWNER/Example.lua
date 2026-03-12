6e23262298e11e53ea99b7bcd9b6ea5cc9f19f9e
arkanzulfadliputra-oss <arkanzulfadliputra@gmail.com>
2026-03-13 07:43:21 +0800

    Create Example.lua

---
+++ Game/DOORS/ITEM SPAWNER/Example.lua
@@ -0,0 +1,9 @@
+local toolModule = loadstring(game:HttpGet("raw link script module lo"))()
+local crucifix = toolModule.create({
+    Asset = "12017043527",
+    Name = "Example",
+    Parent = game.Players.LocalPlayer.Backpack,
+    Icon = "1234567890",
+    Grip = CFrame.new(0, -0.5, 0) * CFrame.Angles(math.rad(90), 0, 0),
+    Anim = "10479585177"
+})
